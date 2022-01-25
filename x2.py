99ceb00a0d924247598787be60cd9d280385d29e
alinashaymardanova <98107093+alinashaymardanova@users.noreply.github.com>
2022-01-25 23:43:03 +0300

    Update x2.py

--- x2.py
+++ x2.py
@@ -1,8 +1,9 @@
-list1 = []
-def collatz(x):
-    pass
-
 def x2(x):
     x = x / 2
+    list1.append(x)
+    return collatz(x)
+
+def x3_1(x):
+    x = x*3+1
     list1.append(x)
     return collatz(x)
