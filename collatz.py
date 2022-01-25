07d227b6e557d155a7c22ff15aef8ae5e9541849
Николай Новиков <nikolien4@gmail.com>
2022-01-25 21:20:15 +0300

    FullFile

--- collatz.py
+++ collatz.py
@@ -1,16 +1,16 @@
 list1 = []
 
 def collatz(x):
-list1.append(x)
-while x != 1:
-    if x == 1:
-        list1.append(1)
-        continue
-    elif x % 2 == 0:
-        return x2(x)
-    else:
-        return x3_1(x)
-    print (list1)
+    list1.append(x)
+    while x != 1:
+        if x == 1:
+            list1.append(1)
+            continue
+        elif x % 2 == 0:
+            return x2(x)
+        else:
+            return x3_1(x)
+        print (list1)
 
 def x2(x):
     x = x / 2
