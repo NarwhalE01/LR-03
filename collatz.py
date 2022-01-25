278d01b0585ed5613e3f7e9be3c7c6d36a34176f
Николай Новиков <nikolien4@gmail.com>
2022-01-25 21:19:07 +0300

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,4 +1,23 @@
+list1 = []
+
+def collatz(x):
+list1.append(x)
+while x != 1:
+    if x == 1:
+        list1.append(1)
+        continue
+    elif x % 2 == 0:
+        return x2(x)
+    else:
+        return x3_1(x)
+    print (list1)
+
 def x2(x):
-  x = x / 2
-  list1.append(x)
-  return collatz(x)
+    x = x / 2
+    list1.append(x)
+    return collatz(x)
+    
+def x3_1(x):
+    x = x * 3 + 1
+    list1.append(x)
+    return collatz(x)
